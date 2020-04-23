4ab4d4c70a87821471275ab0a55e72075bc25c5a
JFMajcher <majcherjakub95@gmail.com>
2020-04-23 21:52:26 +0200

    finished GiveMoves function

--- NNCheckers/Classes.cpp
+++ NNCheckers/Classes.cpp
@@ -8,13 +8,15 @@ using namespace std;
 
 class CheckerBoard {
 public:
+	char pieces[32];
+	
 	void initPieces() {
 		for (int i = 0; i < 32; i++) {
-			pieces[i] = 0;
+			this->pieces[i] = 0;
 		}
 		for (int i = 0; i < 12; i++) {
-			pieces[i] = -1;
-			pieces[31 - i] = 1;
+			this->pieces[i] = -1;
+			this->pieces[31 - i] = 1;
 		}
 	}
 	void initBoard() {
@@ -55,27 +57,47 @@ public:
 
 private:
 	char board[8][8];
-	char pieces[32];
+	//char pieces[32];
 	char piecesCopy[32];
-	vector<char[32]> moves;
+	//vector<char[32]> moves;
 };
 
 
 class Move : public CheckerBoard {
 public:
-	vector<vector<int>> positions; //positions[0] contains indexes of visited fields, positions[1] indexes of take pieces
+	vector<vector<int>> positions; //positions[0] contains indexes of visited fields, positions[1] indexes of taken pieces
 	Move() {}
 	Move(int init_p, int dest_p) {
-		positions[0].push_back(init_p);
-		positions[0].push_back(dest_p);
+		vector<int> positions1;
+		positions1.push_back(init_p);
+		positions1.push_back(dest_p);
+		positions.push_back(positions1);
+		vector<int> takenPieces;
+		positions.push_back(takenPieces);
+	}
+	Move(vector<vector<int>> pos) {
+		positions = pos;
 	}
 
-	Move ActuateJump(Move move, int next_position, int taken_piece) {
-		Move actuated_jump(move[0][0], move[0][1]);
+
+	void ReassignMove(int position, int destination) {
+		this->positions.clear();
+		this->positions[0].push_back(position);
+		this->positions[0].push_back(destination);
+	}
+	void ReassignJump(int position, int destination, int taken_piece) {
+		this->positions.clear();
+		this->positions[0].push_back(position);
+		this->positions[0].push_back(destination);
+		this->positions[1].push_back(taken_piece);
+	}
+	Move ActuateJump(int next_position, int taken_piece) {
+		Move actuated_jump(this->positions[0][0], this->positions[0][1]);
 		actuated_jump[0].push_back(next_position);
 		actuated_jump[1].push_back(taken_piece);
 		return actuated_jump;
 	}
+	
 	//czy to tu bêdê potrzebowa³ wskazników?
 	vector<int> operator [] (int x) {
 		if (x == 0) {
--- NNCheckers/NNCheckers.cpp
+++ NNCheckers/NNCheckers.cpp
@@ -11,12 +11,26 @@
 
 using namespace std;
 
+template <typename T>
+std::vector<T>& operator+=(std::vector<T>& a, const std::vector<T>& b)
+{
+	a.insert(a.end(), b.begin(), b.end());
+	return a;
+}
+
+template <typename T>
+std::vector<T>& operator+=(std::vector<T>& aVector, const T& aObject)
+{
+	aVector.push_back(aObject);
+	return aVector;
+}
 
 
 //FUNCTIONS DECLARATIONS
 vector<int> GiveDiagonals(int x);
 vector<Move> GiveMoves(char pieces[32]);
 int SingleDiagonal(int, int);
+vector<Move> NextJumps(Move, char[32]);
 
 int main()
 {
@@ -24,6 +38,16 @@ int main()
 	cout << test << endl;
 	CheckerBoard checkerBoard;
 	checkerBoard.initPieces();
+	cout << sizeof(checkerBoard.pieces) << endl;
+	int test_d = SingleDiagonal(4, 8);
+	cout << test_d << endl;
+	/*Move test_move(2, 9);
+	vector<Move> test_jumps;
+	vector<Move> test_jumps = NextJumps(test_move, checkerBoard.pieces);*/
+
+	vector<Move> available_moves = GiveMoves(checkerBoard.pieces);
+	
+	
 	checkerBoard.initBoard();
 	checkerBoard.printBoard();
 }
@@ -58,12 +82,28 @@ vector<int> GiveDiagonals(int x) { //returns vector of diagonal indexes in a for
 int SingleDiagonal(int init_pos, int obstacle_pos) {
 	vector<int> diagonals_1 = GiveDiagonals(init_pos);
 	vector<int> diagonals_2 = GiveDiagonals(obstacle_pos);
-	for (int i = 0; i < 4; i++) {
-		if (diagonals_1[i] == obstacle_pos && diagonals_2[i] != 99) {
-			return diagonals_2[i];
+	if (obstacle_pos != 99) {
+		for (int i = 0; i < 4; i++) {
+			if (diagonals_1[i] == obstacle_pos && diagonals_2[i] != 99) {
+				return diagonals_2[i];
+			}
 		}
-	}
+	}	
 	return 99;
+}
+vector<Move> NextJumps(Move jump, char pieces[32]) {
+	vector<Move> possible_jumps;
+	//int position = jump.positions[0].back();
+	//vector<int> diagonals = GiveDiagonals(position);
+	//int size = jump.positions[0].size();
+	/*for (int d : diagonals) {
+		if (d != 99 && pieces[d] != 0 && SingleDiagonal(position, d) != 99) {
+			if (d != jump.positions[0][size - 1] && !pieces[SingleDiagonal(position, d)]) {
+				possible_jumps.push_back(jump.ActuateJump(SingleDiagonal(position, d), d));
+			}			
+		}
+	}*/
+	return possible_jumps;
 }
 vector<Move> GiveMoves(char pieces[32]) {
 	vector<Move> v_moves;
@@ -74,13 +114,41 @@ vector<Move> GiveMoves(char pieces[32]) {
 			v_dials = GiveDiagonals(i);
 			for (int d : v_dials) {
 				if (d != 99) {
-					if (pieces[d] && !pieces[SingleDiagonal(i , d)]) {
-						Move current_move(i, SingleDiagonal(i, d));
-						//v_moves.push_back(current_move); nie da rady tutaj
+					if (pieces[d] && SingleDiagonal(i, d) != 99) {
+						if (!pieces[SingleDiagonal(i, d)]) {
+							current_move.ReassignJump(i, SingleDiagonal(i, d), d);
+							v_moves.push_back(current_move);
+						}						 
 					}
 				}
 			}
 		}
 	}
-	return v_moves;
+	if (v_moves.empty()) {
+		for (int i = 0; i < 32; i++) {
+			if (pieces[i] == 1) {
+				v_dials = GiveDiagonals(i);
+				for (int d : v_dials) {
+					if (d != 99) {
+						if (!pieces[d]) {
+							current_move.ReassignMove(i, d);
+							v_moves.push_back(current_move);
+						}
+					}
+				}
+			}
+		}
+		return v_moves;
+	}
+	else {
+		vector<Move>::iterator it_end = v_moves.end();
+		for (vector<Move>::iterator it = v_moves.begin(); it != it_end; ++it) {
+			if (!NextJumps(*it, pieces).empty()) {
+				v_moves += NextJumps(*it, pieces);
+				v_moves.erase(it);
+			}
+			it_end = v_moves.end();
+		}
+		return v_moves;
+	}
 }
